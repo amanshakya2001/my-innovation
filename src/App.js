f53c27fca5bc68cbfa00d5b61f00d294f3a6012a
Aman <aman.shakya@analyticsvidhya.com>
2024-10-25 21:21:09 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App4.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header className="bg-dark py-2 fixed-top">
+        <div class="container">
+          <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+              <a class="navbar-brand text-white" href="#">My Innovation</a>
+            </div>
+          </nav>
+        </div>
       </header>
-    </div>
+      <section className="welcome vh-100 d-flex align-items-center">
+        <div className="container">
+          <div className="row">
+            <h1 className="text-center">Welcome to My Innovation</h1>
+          </div>
+        </div>
+      </section>
+      <footer className="bg-dark text-center text-white py-1 fixed-bottom">
+          <div className="container">
+              <p className="mb-1">
+                  &copy; 2024 Aman Shakya | Made with <span class="text-danger">❤️</span> for you!
+              </p>
+              <p className="small">Thank you for being a part of our journey. Stay inspired and connected!</p>
+          </div>
+      </footer>
+    </main>
   );
 }
 
